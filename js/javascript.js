6a346758805d0475b9a0453eccbbcdfc4e9c84f7
valera1405 <74429800+valera1405@users.noreply.github.com>
2020-12-12 14:53:30 +0300

    master

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,25 @@
-window.addEventListener ('load', loader, false);
-function loader (e) {
-  e.preventDefault();
-  document.getElementById('vvod-stroki').addEventListener ('submit', validateBukvy, false);
-}
-function validateBukvy(e) {
-  var VvodStroki = [];
-  VvodStroki[01] = "Первая строка: "
-  VvodStroki[01][m] = "М";
-  VvodStroki[01][n] = "Н";
-  VvodStroki[01][k] = "К";
-}
+
+var Stroka01_sh = document.getElementById("stroka-01-sh");
+var Stroka01_b = document.getElementById("stroka-01-b");
+
+var el_down = document.getElementById("GFG_DOW");
+
+function gfg_Run()
+{
+  var a = Stroka01_sh.value;
+  var b = PrStroka01_sh.value;
+  var ans = a.localeCompare(b);
+  var res = "";
+
+  if (ans == -1) {
+    res = '"' + a + '"Comes before"' + b +'"';
+  }
+  else if (ans == 0) {
+    res = 'Both string are same';
+  }
+  else {
+    res = '"' + a + '" comes after "' + b + '"';
+  }
+  el_down.innerHTML = res;
+  }
+
