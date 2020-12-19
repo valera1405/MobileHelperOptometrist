0734e9844f0cc275310e88b1f6b126a03cd5ca02
valera1405 <74429800+valera1405@users.noreply.github.com>
2020-12-19 15:16:30 +0300

    master

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,28 @@
-
 var Stroka01_sh = document.getElementById("stroka-01-sh");
 var PrStroka01_sh = ['Ш'];
 
 var el_down = document.getElementById("GFG_DOW");
 
-function gfg_Run()
-{
+function gfg_Run() {
   var a = Stroka01_sh.value;
   var b = PrStroka01_sh.value;
   var ans = a.localeCompare(b);
   var res = "";
 
   if (ans == -1) {
-    res = '"' + a + '"Comes before"' + b +'"';
-  }
-  else if (ans == 0) {
+    res = '"' + a + '"Comes before"' + b + '"';
+  } else if (ans == 0) {
     res = 'Both string are same';
-  }
-  else {
+  } else {
     res = '"' + a + '" comes after "' + b + '"';
   }
   el_down.innerHTML = res;
-  }
+}
 
 
 var stroka01 = [];
 stroka01[0] = 'Ш';
 stroka01[1] = 'Б';
+
+
 
